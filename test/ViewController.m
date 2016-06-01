d7f6e0bb12f9c6602674a74ee9608729fc2d40f5
909015430@qq.com <henairui456>
2016-06-01 18:00:15 +0800

    22
    22

---
+++ test/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by leying on 16/6/1.
+//  Copyright © 2016年 leying. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    [self findView];
+}
+
+- (void)findView {
+    NSLog(@"11111");
+    
+     NSLog(@"11111");
+     NSLog(@"11111");
+     NSLog(@"11111");
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+}
+
+@end
---
+++ test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by leying on 16/6/1.
+//  Copyright © 2016年 leying. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
